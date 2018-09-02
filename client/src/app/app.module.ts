5434cfbde72070df2961cfae5fda38b2444dbc70
Penta <kai.pentao.o@gmail.com>
2018-09-02 10:51:34 +0300

    Client: Created all page application. Finish category page (list category, create, edit, delete) and list positions (create, edit, delete).

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,16 +3,28 @@ import {NgModule} from '@angular/core';
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
-import {AppComponent} from './app.component';
-import {LoginPageComponent} from './login-page/login-page.component';
-import {AppRoutingModule} from "./app-routing.module";
-import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
-import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
-import { RegisterPageComponent } from './register-page/register-page.component';
 
+import {AppRoutingModule} from "./app-routing.module";
 import {AuthService} from "./shared/services/auth.service";
 import {AuthGuard} from "./shared/classes/auth.guard";
 import {TokenInterceptor} from "./shared/classes/token.interceptor";
+
+import {AppComponent} from './app.component';
+
+import {AuthLayoutComponent} from './shared/layouts/auth-layout/auth-layout.component';
+import {SiteLayoutComponent} from './shared/layouts/site-layout/site-layout.component';
+import {RegisterPageComponent} from './register-page/register-page.component';
+import {LoginPageComponent} from './login-page/login-page.component';
+import {OverviewPageComponent} from './overview-page/overview-page.component';
+import {AnalyticsPageComponent} from './analytics-page/analytics-page.component';
+import {HistoryPageComponent} from './history-page/history-page.component';
+import {OrderPageComponent} from './order-page/order-page.component';
+import {CategoriesPageComponent} from './categories-page/categories-page.component';
+import {CategoriesService} from "./shared/services/categories.service";
+import { LoaderComponent } from './shared/components/loader/loader.component';
+import { CategoriesFormComponent } from './categories-page/categories-form/categories-form.component';
+import { PositionsFormComponent } from './categories-page/categories-form/positions-form/positions-form.component';
+import {PositionsService} from "./shared/services/positions.service";
 
 
 @NgModule({
@@ -21,7 +33,15 @@ import {TokenInterceptor} from "./shared/classes/token.interceptor";
         AuthLayoutComponent,
         SiteLayoutComponent,
         LoginPageComponent,
-        RegisterPageComponent
+        RegisterPageComponent,
+        OverviewPageComponent,
+        AnalyticsPageComponent,
+        HistoryPageComponent,
+        OrderPageComponent,
+        CategoriesPageComponent,
+        LoaderComponent,
+        CategoriesFormComponent,
+        PositionsFormComponent
     ],
     imports: [
         BrowserModule,
@@ -37,7 +57,9 @@ import {TokenInterceptor} from "./shared/classes/token.interceptor";
             provide: HTTP_INTERCEPTORS,
             multi: true,
             useClass: TokenInterceptor
-        }
+        },
+        CategoriesService,
+        PositionsService
     ],
     bootstrap: [AppComponent]
 })
